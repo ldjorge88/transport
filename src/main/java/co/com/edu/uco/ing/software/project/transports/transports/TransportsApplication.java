3027c604eca5064282dc1a7179f40d1affd68165
ldjorge1988@gmail.com <ldjorge1988@gmail.com>
2022-03-09 00:51:34 -0500

    creacion de proyecto base

---
+++ src/main/java/co/com/edu/uco/ing/software/project/transports/transports/TransportsApplication.java
@@ -0,0 +1,13 @@
+package co.com.edu.uco.ing.software.project.transports.transports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/edu/uco/ing/software/project/transports/transports/TransportsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.edu.uco.ing.software.project.transports.transports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
