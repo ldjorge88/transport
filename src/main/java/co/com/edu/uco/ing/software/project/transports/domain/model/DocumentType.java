0702d2368dd5c022cd04e177fc3758dd597777cb
ldjorge1988@gmail.com <ldjorge1988@gmail.com>
2022-03-10 11:10:07 -0500

    adicion de lombok y modelo para tipo de documento

---
+++ src/main/java/co/com/edu/uco/ing/software/project/transports/domain/model/DocumentType.java
@@ -0,0 +1,15 @@
+package co.com.edu.uco.ing.software.project.transports.domain.model;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(staticName="of")
+public class DocumentType {
+
+  private int id;
+  @NonNull
+  private String code;
+  @NonNull
+  private String value;
+
+}
--- src/main/java/co/com/edu/uco/ing/software/project/transports/domain/model/Owner.java
+++ src/main/java/co/com/edu/uco/ing/software/project/transports/domain/model/Owner.java
@@ -33,6 +33,7 @@ public class Owner {
     validateObjectRequired(owner.phone, REQUIRED_FIELD_NULL_EMPTY);
     validateObjectRequired(owner.licensePlate, REQUIRED_FIELD_NULL_EMPTY);
 
+
   }
 
   public static Owner ownerBuilder(int document, String documentType) {
