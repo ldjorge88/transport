418b49e90b888702a2503c6f1270c9165cc4742e
ldjorge1988@gmail.com <ldjorge1988@gmail.com>
2022-03-10 20:25:46 -0500

    correccion de proyecto

---
+++ src/main/java/co/com/edu/uco/ing/software/project/transports/application/converter/DocumentTypeConverter.java
@@ -0,0 +1,4 @@
+package co.com.edu.uco.ing.software.project.transports.application.converter;
+
+public interface DocumentTypeConverter {
+}
---
+++ src/main/java/co/com/edu/uco/ing/software/project/transports/application/service/owner/documenttype/ApplicationSaveDocumentTypeService.java
@@ -0,0 +1,12 @@
+package co.com.edu.uco.ing.software.project.transports.application.service.owner.documenttype;
+
+
+public class ApplicationSaveDocumentTypeService {
+
+  //private final DocumentTypeConverter documentTypeConverter;
+
+  //public void getDocumentTiype(DocumentTypeDTO documentTypeDTO) {
+    //DocumentType documentType = documentTypeConverter.documentTypeDTOToDocumentType(documentTypeDTO);
+  //}
+
+}
